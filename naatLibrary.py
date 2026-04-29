b3108f3068183df994041e1b1d0ce0bbac495576
Hafiz Ahmad Adil <ahmad.adil.gudgk@gmail.com>
2026-04-29 16:41:19 +0500

    Add files via upload

---
+++ naatLibrary.py
@@ -0,0 +1,5 @@
+naat = {"atif": "https://www.youtube.com/watch?v=qYWbrM4nvXQ&list=RDqYWbrM4nvXQ&start_radio=1",
+        "junaid": "https://www.youtube.com/watch?v=l3hHUwc1cAk&list=RDqYWbrM4nvXQ&index=16",
+        "ali": "https://www.youtube.com/watch?v=yR9ZW4mS_EA&list=RDqYWbrM4nvXQ&index=29",
+        "zohaib": "https://www.youtube.com/watch?v=_UkldZoG6oY&list=RDqYWbrM4nvXQ&index=10"
+        }
